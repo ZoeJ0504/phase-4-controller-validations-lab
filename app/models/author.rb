57223c310893cc42a2307ae23d4e415ac54f7b46
Zoe Mendez <zoemendez316@gmail.com>
2022-09-21 09:36:04 -0700

    Done with assignment

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,13 @@
 class Author < ApplicationRecord
+
+    validates :email, uniqueness: true
+    validate :name_can_not_be_empty
+
+    def name_can_not_be_empty
+        if name == nil
+            errors.add(:name, "can't be blank")
+        end
+    end
+
+
 end
